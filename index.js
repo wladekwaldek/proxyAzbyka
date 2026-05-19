269dd0cd6b2d2fbef1589d5b155c7cc3dfb1faa3
waldek <wladekwaldek21@gmail.com>
2026-05-19 20:10:31 +0200

    bot1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Readable } from "stream";
 import dotenv from "dotenv";
 import cors from "cors";
+import TelegramApi from "node-telegram-bot-api";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const TARGET = process.env.BASE_URL;
 const VERSION = process.env.VERSION;
 const APK_URL = process.env.APK_URL;
 const DOMEN = process.env.DOMEN;
+const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 app.use(express.json());
 app.use(cors({ origin: DOMEN, credentials: true }));
@@ -57,3 +59,34 @@ app.listen(PORT, () => {
     `Mobile proxy running on http://localhost:${PORT}\nVersion: ${VERSION}\nAPK URL: ${APK_URL}`,
   );
 });
+
+if (TOKEN) {
+  const bot = new TelegramApi(TOKEN, { polling: true });
+
+  bot.on("message", async (msg) => {
+    const chatId = msg.chat.id;
+    const date = msg.date;
+    const { id, is_bot, first_name, username, language_cod } = msg.from;
+    if (msg.text && msg.text.trim() !== "") {
+      switch (msg.text) {
+        case "/start":
+          await bot.sendMessage(
+            chatId,
+            `Привет, ${first_name}! Я прокси-сервер для мобильного приложения Азбука Веры. Я могу помочь тебе получить последнюю версию приложения и ответить на твои вопросы. Просто напиши мне!`,
+          );
+          break;
+        case "/version":
+          await bot.sendMessage(
+            chatId,
+            `Последняя версия приложения: ${VERSION}\nСсылка для скачивания: ${APK_URL}`,
+          );
+          break;
+        default:
+          await bot.sendMessage(
+            chatId,
+            `Извини, я не понимаю эту команду. Попробуй /start или /version.`,
+          );
+      }
+    }
+  });
+}
